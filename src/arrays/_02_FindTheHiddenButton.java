b4f20177d32214e9b5d66ada8a43c2e0a52919c6
ArielBaruch <league@WTS14-4.attlocal.net>
2019-04-01 18:28:24 -0700

    hidden button ip
    :sleeping:

--- src/arrays/_02_FindTheHiddenButton.java
+++ src/arrays/_02_FindTheHiddenButton.java
@@ -16,41 +16,49 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class _02_FindTheHiddenButton implements ActionListener{
-	JFrame window;
-	JPanel panel;
+	JPanel panel=new JPanel();
+	JFrame frame=new JFrame();
 	
 	//1. create an array of JButtons. Don't initialize it yet.
-	
+	JButton[] buttons;
 	//2 create an int variable called hiddenButton
-	
+	int hiddenButton;
 	public static void main(String[] args) {
 		new _02_FindTheHiddenButton().start();
+		
 	}
 	
 	public void start() {
-		window = new JFrame("Find the Button");
-		panel = new JPanel();
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		//3.  Ask the user to enter a positive number and convert it to an int
-		
+		String a = JOptionPane.showInputDialog("enter a whole positive number");
+int num=Integer.parseInt(a);
+
 		//4. Initialize the array of JButtons to be the size of the int created in step 3
-		
+buttons= new JButton[num];
 		//5. Make a for loop to iterate through the JButton array
+		for (int i = 0; i < buttons.length; i++) {
+		buttons[i]=new JButton();
+			buttons[i].addActionListener(this);
+			panel.add(buttons[i]);
+		}
 			//6. initialize each JButton in the array
+		
 			//7. add the ActionListener to each JButton
 			//8. add each JButton to the panel
 		
 		//9 add the panel to the window
-		
+		frame.add(panel);
 		//10. call setExtendedState(JFrame.MAXIMIZED_BOTH) on your JFrame object.
-		
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		//11. set the JFrame to visible.
-		
+	frame.setVisible(true);
+	frame.pack();
 		//12. Give the user the instructions for the game.
-		
+		JOptionPane.showMessageDialog(null, "In this game all you have to do it try to guess what button is the secret super amazing button. if you do you get a high 5");
 		//13. initialize the hiddenButton variable to a random number less than the int created int step 3
-		
+		Random rand = new Random();
+		hiddenButton=rand.nextInt(num);
 		//14. Set the text of the JButton located at hiddenButton the read "ME"
 
 		//15. Use Thread.sleep(100); to pause the program.
